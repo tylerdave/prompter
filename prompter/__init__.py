33affd407f5ab6c5259a68c8e20bae7fda5beeb2
Dave Forgac <tylerdave@tylerdave.com>
2014-04-12 02:00:44 -0400

    making it work under python 3.x

--- prompter/__init__.py
+++ prompter/__init__.py
@@ -1,10 +1,24 @@
 from __future__ import print_function
 
 __title__ = 'prompter'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Dave Forgac'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Dave Forgac'
+
+
+# nasty hack while we need to support python 2.7 and 3.x :-(
+try:
+    raw_input
+except NameError:
+    import builtins
+
+    original_input = builtins.input
+    del builtins.input
+    def raw_input(*args, **kwargs):
+        return original_input(*args, **kwargs)
+    builtins.raw_input = raw_input
+
 
 def prompt(message, default=None, strip=True):
     print(message)
@@ -15,8 +29,6 @@ def prompt(message, default=None, strip=True):
 
     input_value = raw_input(prompt_text)
 
-    # kept separate so default is used when strip is True and input is only
-    # spaces. TODO: add test for this case
     if input_value and strip:
         input_value = input_value.strip()
 
