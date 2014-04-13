cc2a24c0e8b0e9d1ada9c082442a77a8ae26580f
Dave Forgac <tylerdave@tylerdave.com>
2014-04-12 22:27:06 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup, find_packages
 
 import prompter
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='prompter',
     version=prompter.__version__,
     description='Simple CUI input prompt',
+    long_description=readme,
     author='Dave Forgac',
     author_email='tylerdave@tylerdave.com',
     url='https://github.com/tylerdave/prompter',
