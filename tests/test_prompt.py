165a2bf48160d4bcc2ea45a7fca48f91f56dd000
Marshall Asch <maasch@rogers.com>
2022-07-31 20:42:26 -0400

    feat: replaced noose with modern pytest

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -1,31 +1,30 @@
 from mock import patch
-from nose.tools import assert_equals
 from prompter import prompt
 
 @patch('prompter.get_input', return_value='Dave')
 def test_prompt_returns_input(mock_raw_input):
     returned_value = prompt('What is your name?')
-    assert_equals(returned_value, 'Dave')
+    assert returned_value == 'Dave'
 
 @patch('prompter.get_input', return_value='')
 def test_prompt_returns_default_for_blank_input(mock_raw_input):
     returned_value = prompt('What is your name?', default='Dave')
-    assert_equals(returned_value, 'Dave')
+    assert returned_value == 'Dave'
 
 @patch('prompter.get_input', return_value=' Dave  ')
 def test_prompt_strips_whitespace(mock_raw_input):
     returned_value = prompt('What is your name?')
-    assert_equals(returned_value, 'Dave')
+    assert returned_value == 'Dave'
 
 @patch('prompter.get_input', return_value=' Dave  ')
 def test_prompt_does_not_strip_whitespace(mock_raw_input):
     returned_value = prompt('What is your name?', strip=False)
-    assert_equals(returned_value, ' Dave  ')
+    assert returned_value == ' Dave  '
 
 @patch('prompter.get_input', return_value='        ')
 def test_prompt_returns_default_with_only_whitespace_input(mock_raw_input):
     returned_value = prompt('What is your name?', default='Dave')
-    assert_equals(returned_value, 'Dave')
+    assert returned_value == 'Dave'
 
 
 
